a3f00e6df2f4190ff075f002a2049b751a95245b
SHEVISANTOS <shevijeremiah@gmail.com>
2026-07-22 21:42:47 +0300

    vercel deployment

--- vercel_app.py
+++ vercel_app.py
@@ -1,5 +1,8 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "must_mrejesho.settings")
-application = get_wsgi_application()
+# This MUST match your inner folder name: must_mrejesho
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'must_mrejesho.settings')
+
+# Vercel looks for 'app' or 'application'
+app = get_wsgi_application()
