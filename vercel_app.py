ebe39d638319b0ef69354523540ba9ec8454da1f
SHEVISANTOS <shevijeremiah@gmail.com>
2026-07-23 16:57:04 +0300

    Fix WhiteNoise routing for Vercel

--- vercel_app.py
+++ vercel_app.py
@@ -2,14 +2,14 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-# Set the default settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'must_mrejesho.settings')
 
-# Get the Django WSGI application
 application = get_wsgi_application()
 
-# Wrap it with WhiteNoise to serve static files
-# Note: 'staticfiles' is the folder where collectstatic puts files
-application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'staticfiles'))
+# Explicitly point WhiteNoise to the staticfiles folder
+application = WhiteNoise(
+    application, 
+    root=os.path.join(os.path.dirname(__file__), 'staticfiles')
+)
 
 app = application
