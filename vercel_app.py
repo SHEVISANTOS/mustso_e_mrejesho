f6925665e1e4b4ece66f5f64d30a02d1270c8ec2
SHEVISANTOS <shevijeremiah@gmail.com>
2026-07-23 16:31:05 +0300

    Add static files for admin

--- vercel_app.py
+++ vercel_app.py
@@ -1,8 +1,15 @@
 import os
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
-# This MUST match your inner folder name: must_mrejesho
+# Set the default settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'must_mrejesho.settings')
 
-# Vercel looks for 'app' or 'application'
-app = get_wsgi_application()
+# Get the Django WSGI application
+application = get_wsgi_application()
+
+# Wrap it with WhiteNoise to serve static files
+# Note: 'staticfiles' is the folder where collectstatic puts files
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'staticfiles'))
+
+app = application
